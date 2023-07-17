7bcb64a8f5987465e888c1442b7c967a8def82d2
berry <93798401+berry-shop@users.noreply.github.com>
2023-07-17 22:08:23 +0300

    Delete js directory

--- js/callbackModal.js
+++
@@ -1,6 +0,0 @@
-const callbackModal = document.querySelector('.callback-modal');
-const callbackOpen = document.querySelector('.callback-open');
-
-callbackOpen.addEventListener('click', () => {
-  callbackModal.classList.toggle('callback-modal-active');
-});
