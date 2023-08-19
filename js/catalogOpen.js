235c0133d046395338364467c8cdebd6f2b0bbc0
berry <93798401+berry-shop@users.noreply.github.com>
2023-08-19 22:43:35 +0300

    Delete js directory

--- js/catalogOpen.js
+++
@@ -1,7 +0,0 @@
-const cartOpen = document?.querySelector('.cart__img');
-const cartContent = document?.querySelector('.cart__content')
-
-
-cartOpen?.addEventListener('click', () => {
-   cartContent.classList.toggle('cart--active');
- });
